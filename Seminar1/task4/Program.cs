e5215ae310b4dc7e1172b9a3f5bcfe724f919516
Nemeni Natalia <nnemeni80@gmail.com>
2023-02-02 00:55:15 +0300

    Added folder Seminar1/task4

---
+++ Seminar1/task4/Program.cs
@@ -0,0 +1,33 @@
+﻿System.Console.WriteLine ("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите третье число");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (numberB>numberA)
+{
+  if (numberB>numberC)
+  {
+   System.Console.Write("Max=");
+   System.Console.WriteLine(numberB);
+  }
+  else
+  {
+   System.Console.Write("Max=");
+   System.Console.WriteLine(numberC);
+  }
+}
+else
+{
+  if (numberA>numberC)
+  {
+    System.Console.Write("Max=");
+    System.Console.WriteLine(numberA);   
+  }
+  else
+  {
+    System.Console.Write("Max=");
+    System.Console.WriteLine(numberC);   
+  }
+}
+   
