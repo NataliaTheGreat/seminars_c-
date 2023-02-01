31b1e72b31c2e654fff379a466f82349c30fbc25
Nemeni Natalia <nnemeni80@gmail.com>
2023-02-02 01:29:13 +0300

    added task6

---
+++ Seminar1/task6/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine ("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int a =2;
+int b = number% a;
+if (b==0)
+{
+   System.Console.Write("ДА");
+}
+else 
+{
+System.Console.Write("НЕТ");
+}
