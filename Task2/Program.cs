7eda13898abd202ee61b3399fe9a355cb9f788a3
Nemeni Natalia <nnemeni80@gmail.com>
2023-02-02 00:27:53 +0300

    rename Folder Sem1 to Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine ("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB)
+{
+   System.Console.Write("Max=");
+   System.Console.WriteLine (numberA);
+   System.Console.Write("Min=");
+   System.Console.WriteLine (numberB);
+}
+else 
+{
+   System.Console.Write("Max=");
+   System.Console.WriteLine (numberB);
+   System.Console.Write("Min=");
+   System.Console.WriteLine (numberA);
+}
