981e72f0891e20ce4900a6ca7864b064e57be286
Nemeni Natalia <nnemeni80@gmail.com>
2023-02-02 14:19:06 +0300

    Добавили task 8

---
+++ Seminar1/task8/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine ("Введите число > 0");
+int number = Convert.ToInt32(Console.ReadLine());
+int b = number% 2; //остаток от деления
+if(number<0)
+{
+   System.Console.WriteLine ("Вы ввели отрицательное число");
+   System.Console.WriteLine ("Введите число > 0");
+}
+System.Console.Write("Вcе четные числа от 1 до ");  
+System.Console.WriteLine(number);   
+for(int i =1; i < number; i++)
+{
+  if(i % 2 == 0)
+   {
+   System.Console.Write(i);
+   System.Console.Write(' ');
+   }
+  
+}
+
