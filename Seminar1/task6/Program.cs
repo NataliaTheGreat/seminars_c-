0af5b7668ae33e72b3eedafe25699efca8b1651b
Nemeni Natalia <nnemeni80@gmail.com>
2023-02-02 14:08:56 +0300

    Changed programm task 6

--- Seminar1/task6/Program.cs
+++ Seminar1/task6/Program.cs
@@ -1,20 +1,10 @@
-﻿System.Console.WriteLine ("Введите число > 0");
+﻿System.Console.WriteLine ("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
-int b = number% 2; //остаток от деления
-if(number<0)
+if (number% 2==0)
 {
-   System.Console.WriteLine ("Вы ввели отрицательное число");
-   System.Console.WriteLine ("Введите число > 0");
+   System.Console.Write("ДА");
 }
-System.Console.Write("Вcе четные числа от 1 до ");  
-System.Console.WriteLine(number);   
-for(int i =1; i < number; i++)
+else 
 {
-  if(i % 2 == 0)
-   {
-
-   System.Console.WriteLine (i);
-   }
-  
+System.Console.Write("НЕТ");
 }
-
