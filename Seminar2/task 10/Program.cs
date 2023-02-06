a7eba1dbde6d8ef52b3dac513581b27676095ea8
Nemeni Natalia <nnemeni80@gmail.com>
2023-02-06 20:48:01 +0300

    Made code more simple  in task 10

--- Seminar2/task 10/Program.cs
+++ Seminar2/task 10/Program.cs
@@ -3,11 +3,13 @@
 //456 -> 5
 //782 -> 8
 //918 -> 1
+
+
 System.Console.WriteLine ("Введите трехзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number<100) 
 {
-  System.Console.WriteLine ("Это двухзначное число. Перезапустите программу");  
+  System.Console.WriteLine ("Это не трезначное число. Перезапустите программу");  
 }
 else
   if (number>999) 
@@ -16,8 +18,6 @@ else
   }
   else
   {
-  int number2 = number/10;
-  //System.Console.WriteLine (number2);
-  System.Console.Write("Вторая цифра числа ");
-  System.Console.WriteLine (number2%10);
-  }
+ 
+  System.Console.WriteLine ($"Вторая цифра числа: {(number/10)%10}"); 
+   }
