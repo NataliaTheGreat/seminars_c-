245bf23d84ea951a645c12dbfb48ac7efa5d2e72
Nemeni Natalia <nnemeni80@gmail.com>
2023-02-06 20:09:12 +0300

    added task 11 and task 13

---
+++ Seminar2/task 11/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+System.Console.WriteLine ("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<100) 
+{
+  System.Console.WriteLine ("Это двухзначное число. Перезапустите программу");  
+}
+else
+  if (number>999) 
+  {
+   System.Console.WriteLine ("Это не трезначное число. Перезапустите программу");  
+  }
+  else
+  {
+  int number2 = number/100;
+  int number3 = number%10;
+  System.Console.WriteLine ("Удалили вторую цифру и получили"); 
+  System.Console.Write (number2);
+  System.Console.Write (number3);
+  }
+
+
---
+++ Seminar2/task 13/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+System.Console.WriteLine ("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<100) 
+ {
+  System.Console.WriteLine ("Третьей цифры нет");  
+ }
+else
+  {
+  int number2 = number/10;
+  //System.Console.WriteLine (number2);
+  System.Console.Write("Вторая цифра числа ");
+  System.Console.WriteLine (number2%10);
+  }
