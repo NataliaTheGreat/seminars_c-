36d21dba84b40ec1449beb19c578236cccbdaa6e
Nemeni Natalia <nnemeni80@gmail.com>
2023-02-06 19:32:29 +0300

    Added Folder Seminar2

---
+++ Seminar2/task 10/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+System.Console.WriteLine ("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<100) 
+{
+  System.Console.WriteLine ("Это двухзначное число. Перезапустите программу");  
+}
+else
+  if (number>999) 
+  {
+   System.Console.WriteLine ("Это не трезначное число. Перезапустите программу");  
+  }
+  else
+  {
+  int number2 = number/10;
+  //System.Console.WriteLine (number2);
+  System.Console.Write("Вторая цифра числа ");
+  System.Console.WriteLine (number2%10);
+  }
