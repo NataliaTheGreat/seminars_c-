88aaa52564e718ed3c729d07889901b0dbe62858
Nemeni Natalia <nnemeni80@gmail.com>
2023-02-20 21:19:53 +0300

    added task 27

---
+++ Seminar4/task 27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int GetNumber(string text)
+{
+System.Console.Write(text);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSumm(int N)
+{
+int result = 0;
+
+while (N%10 > 0)
+{
+result = result + N%10; 
+N=N/10;
+}
+result = result + N/10;
+return result;
+}
+
+int N = GetNumber("Введите число N: ");
+int res = GetSumm(N);
+System.Console.Write($"Сумма цифр числа = {res}");
