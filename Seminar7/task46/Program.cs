b47d71143ad985e9565f43d6ecbdbd78de9cbd30
Nemeni Natalia <nnemeni80@gmail.com>
2023-03-09 15:24:01 +0300

    Modified code

--- Seminar7/task46/Program.cs
+++ Seminar7/task46/Program.cs
@@ -6,39 +6,39 @@
 
 int ReadInt(string text)
 {
-System.Console.Write(text);
-return Convert.ToInt32(Console.ReadLine());
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
 }
 
 int[,] GenerateMatrix(int rows, int cols)
 {
-Random rand = new Random();
-int[,] matrix = new int[rows, cols];
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
 
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-for (int j = 0; j < matrix.GetLength(1); j++)
-{
-matrix[i, j] = rand.Next(0, 21);
-}
-}
-return matrix;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 21);
+        }
+    }
+    return matrix;
 }
 
 void PrintMatrix(int[,] matrix)
 {
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-for (int j = 0; j < matrix.GetLength(1); j++)
-{
-System.Console.Write(matrix[i,j] + "\t");
-}
-System.Console.WriteLine();
-}
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
 }
 
 int rows = ReadInt("Введите количество строк матрицы: ");
 int cols = ReadInt("Введите количество столбцов матрицы: ");
 System.Console.WriteLine();
-var myMatrix = GenerateMatrix(rows,cols);
+var myMatrix = GenerateMatrix(rows, cols);
 PrintMatrix(myMatrix);
