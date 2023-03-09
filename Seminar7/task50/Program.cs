965c495444216e296804524adaa9fcc402d3f15b
Nemeni Natalia <nnemeni80@gmail.com>
2023-03-09 15:25:17 +0300

    Added task 50

---
+++ Seminar7/task50/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+// в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такой позиции в массиве нет (пользователь вводит нумерацию с 1)
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    int[,] matrix = new int[rows, cols];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+
+void SearchElement(int rowi, int rowj, int[,] matrix)
+{
+    if (rowi < matrix.GetLength(0)
+        && rowj < matrix.GetLength(1)
+        && rowi > 0
+        && rowi > 0)
+    {
+        Console.WriteLine($"Элемент массива с  индексом [{rowi},{rowj}]  = {matrix[rowi, rowj]}");
+
+    }
+    else
+        Console.WriteLine("Такой позиции в массиве нет");
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    
+    }
+        System.Console.WriteLine();
+}
+
+int rows = ReadInt("Введите количество строк матрицы: ");
+int cols = ReadInt("Введите количество столбцов матрицы: ");
+int rowi = ReadInt("Введите индекс элемента i: ");
+int colj = ReadInt("Введите индекс элемента j: ");
+
+var myMatrix = GenerateMatrix(rows, cols);
+PrintMatrix(myMatrix);
+SearchElement(rowi, colj, myMatrix);
