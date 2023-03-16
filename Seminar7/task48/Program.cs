96825a265e263bbd7927857628aae59553dde531
Nemeni Natalia <nnemeni80@gmail.com>
2023-03-16 14:49:29 +0300

    Modifed a little beat

--- Seminar7/task48/Program.cs
+++ Seminar7/task48/Program.cs
@@ -7,33 +7,33 @@
 
 int ReadInt(string text)
 {
-System.Console.Write(text);
-return Convert.ToInt32(Console.ReadLine());
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
 }
 
 int[,] GenerateMatrix(int rows, int cols)
 {
-int[,] matrix = new int[rows, cols];
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-for (int j = 0; j < matrix.GetLength(1); j++)
-{
-matrix[i, j] = i + j;
-}
-}
-return matrix;
+    int[,] matrix = new int[rows, cols];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
 }
 
 void PrintMatrix(int[,] matrix)
 {
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-for (int j = 0; j < matrix.GetLength(1); j++)
-{
-System.Console.Write(matrix[i, j] +"\t");
-}
-System.Console.WriteLine();
-}
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
 }
 
 int rows = ReadInt("Введите количество строк матрицы: ");
