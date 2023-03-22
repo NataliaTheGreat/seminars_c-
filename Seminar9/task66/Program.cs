a56366885488f801be513c23ab3cfc996a686d06
Nemeni Natalia <nnemeni80@gmail.com>
2023-03-22 17:17:45 +0300

    Added Folder Seminar9, tasks 66 and 68

---
+++ Seminar9/task66/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+System.Console.WriteLine("Введите первое число");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumFromMtoN(int m, int n)
+{
+    if (m == n) return n;
+    else return m + SumFromMtoN(m + 1, n);
+}
+
+if (m < n)
+    Console.WriteLine($"Сумма элементов от {m} до {n} равна {SumFromMtoN(m, n)}");
+else
+    Console.WriteLine($"Сумма элементов от {n} до {m} равна {SumFromMtoN(n, m)}");
---
+++ Seminar9/task68/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int Akk(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Akk(n - 1, 1);
+    else
+      return Akk(n - 1, Akk(n, m - 1));
+}
+
+Console.Write("Задайте число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($" A({m},{n})= {(Akk(m,n))}");
+
